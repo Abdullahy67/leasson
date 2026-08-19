5b553c8eefe0623b5672add83a37836244323f96
Abdullahy67 <alkomanabdullah@gmail.com>
2026-08-19 13:01:00 +0300

    my shop code

---
+++ my snack.py
@@ -0,0 +1,15 @@
+snack="chips"
+size= 0.5
+price= 1.50
+print ("I produced the chips for 1.50 dollars and the size is 0.5 ounces the final price is", price+size)
+shop="Snack"
+name="Shack"
+print(shop+ " " + name)
+print (shop[0])
+price_a=2
+price_b=3
+print("The prices are:", price_a, "and", price_b)
+temp=price_a
+price_a=price_b
+price_b=temp
+print("The prices are:", price_a, "and", price_b)
